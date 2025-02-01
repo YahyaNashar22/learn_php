2538fca086b6a9c00a9a228dbba0f2dba0673cb3
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 21:16:26 +0200

    added db configuration

--- code-start/config.php
+++ code-start/config.php
@@ -1,9 +1,16 @@
 <?php
 
+$hostname = "localhost";
+$db_name = "learn_php";
+$db_user = "yahya";
+$db_password = "yahya";
 
+ //database source name
+$dsn = "mysql:host=$hostname;db_name:$dbname";
 
- //database sourse name
-
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+]
 
 
  ?>
--- code-start/install.php
+++ code-start/install.php
@@ -1,10 +1,21 @@
-
+<?php require_once("config.php") ?>
 <?php
 
-
 //Php DataBase Object (PDO)
+$connection = new PDO($dsn, $db_user, $db_password, $options);
 
+$sql = 
+"
+CREATE TABLE IF NOT EXISTS peopleTable(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    biography TEXT NOT NULL
+);
+";
+
+$connection->exec($sql);
 
 echo "You are connected to the database successfully";
 
- ?>
+?>
