c0e767f82a772a43c385ed1ac928aad4075b595c
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 23:39:51 +0200

    added insert statement

--- code-start/index.php
+++ code-start/index.php
@@ -9,31 +9,36 @@
     <meta charset="utf-8">
     <title><?php echo $page_title; ?></title>
     <style>
-    table,
-    th,
-    td {
-        border: 1px solid;
-        border-collapse: collapse;
-    }
+        table,
+        th,
+        td {
+            border: 1px solid;
+            border-collapse: collapse;
+        }
     </style>
 </head>
 
 <body>
     <h1> <?php echo $page_heading; ?> </h1>
 
-    <?php if(isset($_POST['submit'])): ?>
+    <?php
 
-        <?php if(isset($_POST['title'])): ?>
+    $idea_title = '';
+    $idea_text = '';
+
+    if (isset($_POST['submit'])): ?>
+
+        <?php if (isset($_POST['title'])): ?>
             <?php $idea_title = $_POST['title']; ?>
         <?php endif; ?>
 
-        <?php if(isset($_POST['text'])): ?>
-            <?php $idea_text = $_POST['text']; ?>
+        <?php if (isset($_POST['content'])): ?>
+            <?php $idea_text = $_POST['content']; ?>
         <?php endif; ?>
 
 
         <div style="background:green;color:white; padding:10px;">
-            <h3> Your idea was added succesfully:</h3>
+            <h3> Your idea was added successfully:</h3>
             <p>Your idea title: <?php echo $idea_title; ?> </p>
             <p>Your idea text: <?php echo $idea_text; ?> </p>
         </div>
@@ -43,12 +48,39 @@
 
     <?php endif; ?>
 
+    <?php $new_idea = ['title' => $idea_title, 'content' => $idea_text]; ?>
+    <?php $keys_string = implode(',', array_keys($new_idea)); ?>
+
+    <?php $keys_placeholder = ':' . implode(', :', array_keys($new_idea)); ?>
+
+    <?php echo $keys_string; ?>
+    <?php echo $keys_placeholder; ?>
+
+    <?php
+    $connection = new PDO($dsn, $db_user, $db_password, $options);
+
+    $sql = sprintf(
+        "INSERT INTO %s (%s) VALUES (%s)",
+        'ideasTable',
+        $keys_string,
+        $keys_placeholder
+    );
+
+    $statement = $connection->prepare($sql);
+    $statement->execute($new_idea);
+
+    ?>
+
     <form action="" method="POST">
         <label for="title">Idea Title</label>
-        <input type="text" name="title" value="<?php if(isset($_POST['title'])){echo $_POST['title'];} ?>">
+        <input type="text" name="title" value="<?php if (isset($_POST['title'])) {
+                                                    echo $_POST['title'];
+                                                } ?>">
         <br><br>
         <label for="text"> Idea Text</label>
-        <textarea name="text" rows="8" cols="80"><?php if(isset($_POST['text'])){echo $_POST['text'];} ?></textarea>
+        <textarea name="content" rows="8" cols="80"><?php if (isset($_POST['content'])) {
+                                                        echo $_POST['content'];
+                                                    } ?></textarea>
         <br><br>
         <input type="submit" name="submit" value="Save your idea">
     </form>
@@ -57,12 +89,12 @@
     <hr><br>
 
 
-    <?php 
-        $connection = new PDO($dsn, $db_user, $db_password, $options);
-        $sql = "SELECT * FROM ideasTable;";
-        $statement = $connection->prepare($sql);
-        $statement->execute();
-        $results = $statement-> fetchAll();
+    <?php
+    $connection = new PDO($dsn, $db_user, $db_password, $options);
+    $sql = "SELECT * FROM ideasTable;";
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $results = $statement->fetchAll();
     ?>
 
 
@@ -75,12 +107,12 @@
         </tr>
 
         <?php foreach ($results as $row): ?>
-        <tr>
-            <th><a href="update.php"><?php echo $row["id"]; ?></a></th>
-            <th><?php echo $row["title"]; ?></th>
-            <th><?php echo $row["content"]; ?></th>
-            <th><a href="delete.php" style="color:red;">X</a></th>
-        </tr>
+            <tr>
+                <th><a href="update.php"><?php echo $row["id"]; ?></a></th>
+                <th><?php echo $row["title"]; ?></th>
+                <th><?php echo $row["content"]; ?></th>
+                <th><a href="delete.php" style="color:red;">X</a></th>
+            </tr>
         <?php endforeach; ?>
 
     </table>
