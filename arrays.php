e429a9ede158964b5b044a2abd3333e4f26db34c
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 11:18:42 +0200

    explode and implode

--- arrays.php
+++ arrays.php
@@ -3,6 +3,10 @@
 $numbers = [1, 2, 3, 4, 5];
 $strings = ["person 1" => "yahya", "person 2" =>  "way", "family 1" => "nashar", "family 2" => "ghannoum"];
 
-asort($strings);
+$str = "This is a string that is going to be exploded";
+$exploded = explode(" ", $str);
+$imploded = implode("-", $exploded);
 
-print_r($strings);
+print_r($imploded);
+
+asort($strings);
