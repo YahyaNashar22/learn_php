5f3274082412d913b4bad4b60446710ccca9ad41
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 12:20:46 +0200

    interfaces

--- inheritance.php
+++ inheritance.php
@@ -1,12 +1,22 @@
 <?php
 
-class A {
+interface printAge
+{
+    function displayAge(): void;
+}
+
+class A implements printAge
+{
     public $age = 23;
+
+    function displayAge(): void
+    {
+        echo "$this->age\n";
+    }
 }
 
-class B extends A{
+class B extends A implements printAge {}
 
-}
 
 $a = new A();
 $b = new B();
@@ -17,3 +27,6 @@ $b->age = 12;
 echo $b->age;
 echo "\n";
 echo $a->age;
+echo "\n";
+
+$b->displayAge();
