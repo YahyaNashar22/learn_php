77402f19504edfc53caea141319f39ff55a2ff1d
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 12:06:45 +0200

    constructor

--- oop.php
+++ oop.php
@@ -4,9 +4,17 @@
 class Human
 {
     private $age;
+    private $name;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
 
     public function info()
     {
+        echo "Name: $this->name  \n";
         echo "Age: $this->age  \n";
     }
 
@@ -21,10 +29,13 @@ class Human
     }
 }
 
-$h1 = new Human();
+$h1 = new Human("child", 10);
+$h2 = new Human("yahya", 26);
 
 $h1->getAge();
 
 $h1->setAge(23);
 
 $h1->getAge();
+
+$h2->info();
