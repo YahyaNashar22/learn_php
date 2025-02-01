0610e01099324b68300f409536407b2d3fdf696a
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 11:44:43 +0200

    functions

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function php_is_shit($name)
+{
+    echo "You are right $name Php is SHIT!\n";
+}
+
+function i_can_return()
+{
+    return "This language is stupid\n";
+}
+
+php_is_shit("yahya");
+$truth = i_can_return();
+
+echo $truth;
