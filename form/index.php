a24d6172983d90dbdeb947442fbe46e2a544046e
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 13:01:00 +0200

    and

--- form/index.php
+++ form/index.php
@@ -8,15 +8,21 @@
 </head>
 
 <body>
-    <form action="hello.php">
+    <form action="" method="GET">
         <label for="name">Name</label>
         <input type="text" name="name" placeholder="Enter your Name" />
         <hr />
         <label for="bio">Biography</label>
-        <textarea name="bio" id="" cols="30" rows="10">Your Biography</textarea>
+        <textarea name="bio" id="" cols="30" rows="10" placeholder="Enter your Biography"></textarea>
         <hr />
         <input type="submit" name="submit" value="Save your profile" />
     </form>
+
+    <?php
+
+    if (isset($_GET["name"])) echo ("{$_GET["name"]} <br />");
+    if (isset($_GET["bio"])) echo ("{$_GET["bio"]}<br />");
+    ?>
 </body>
 
 </html>
