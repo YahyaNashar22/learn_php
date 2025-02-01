8f78adc0aeb93601c4a18761a3dc044776272e75
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 10:29:59 +0200

    basic variable declarations

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello word";
+$number = 12;
+$my_name = "yahya";
+$is_bad = false;
+$is_bad_hm = true;
+$my_array = ["test", "sets"];
+$my_array_legacy = array("index1", "index2");
+
+echo $number;
+echo $my_name;
+echo $is_bad;
+echo $is_bad_hm;
+print_r($my_array);
+print_r($my_array_legacy);
+?>
