6b75eab4e09bfdebccb4ef7caa0b43d62395a6c1
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 12:02:34 +0200

    public private setter getter

--- oop.php
+++ oop.php
@@ -3,16 +3,28 @@
 
 class Human
 {
-    public $age= 10;
+    private $age;
 
     public function info()
     {
         echo "Age: $this->age  \n";
     }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        echo "The age is $this->age\n";
+    }
 }
 
 $h1 = new Human();
-// $h1->age = 22;
 
-echo $h1->age;
-?>
+$h1->getAge();
+
+$h1->setAge(23);
+
+$h1->getAge();
