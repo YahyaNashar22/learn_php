f8c03cfce0f9c1550b3dd2d113308343f337c39b
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-01 11:48:33 +0200

    recursion

--- functions.php
+++ functions.php
@@ -10,7 +10,14 @@ function i_can_return()
     return "This language is stupid\n";
 }
 
+function recursion($n)
+{
+    if ($n !=0 ) return $n + recursion($n- 1);
+    return 0;
+}
+
 php_is_shit("yahya");
 $truth = i_can_return();
 
-echo $truth;
+$recurred = recursion(3);
+echo $recurred;
